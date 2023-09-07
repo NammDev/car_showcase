fa2959b84e8f80de239e3eaedb203eeda60c1102
Nam <namdeveloper.ca@gmail.com>
2023-09-07 22:01:46 +0700

    Car Catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
+import CustomFilter from '@/components/CustomFilter'
 import Hero from '@/components/Hero'
+import SearchBar from '@/components/SearchBar'
+import { fuels, yearsOfProduction } from '@/constants'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {/* 
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )} */}
+      </div>
     </main>
   )
 }
